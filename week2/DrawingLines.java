ae9c3d53016b1e0be28e972e39ac5e44bacb8c6e
yourUsername <yourEmail@domain.com>
2021-02-21 11:14:54 -0500

    HH

--- week2/DrawingLines.java
+++ week2/DrawingLines.java
@@ -1,35 +1,69 @@
 package week2;
 import java.awt.*;
+import java.awt.event.MouseEvent;
 
 import acm.graphics.GLine;
 import acm.program.GraphicsProgram;
 
 
+
 public class DrawingLines extends GraphicsProgram{
 
 	
-	
-	
-	public void paint(Graphics g){
-		 
-		g.drawLine(0, 0, 100, 100);
-		
-	}
+	public void run() {
+       
+		addMouseListeners();
+    }
+    // mouth press  to start a line
+   
+	//reposition for the mouse drag
+	   
+		public void mouseDragged(MouseEvent cursor) {
+	        double x = cursor.getX();
+	        
+	        
+	        double y = cursor.getY();
+	       
+	        
+	        
+	        line.setEndPoint(x, y);
+	    
+		}
 
 	
 	
 	
-	public void run() {
-		
-		
-		
-		
+	
+	public void mousePressed(MouseEvent cursor) {
+        double x = cursor.getX();
+       
+        
+        
+        double y = cursor.getY();
+       
+        
+        line = new GLine(x, y, x, y);
+      
+        add(line);
+  
 	}
+
+   //reposition for the mouse drag
+   
 	
-	
-	
-	
+
+    private GLine line;
+
 }
+
+
+	
+	
+	
+	
+	
+	
+
 
 
  
