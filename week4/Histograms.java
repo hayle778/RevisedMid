f7bc4f161ca3cecedd181deb58b66d9d0d1e83d1
yourUsername <yourEmail@domain.com>
2021-03-08 16:25:07 -0500

    gg

--- week4/Histograms.java
+++ week4/Histograms.java
@@ -8,4 +8,9 @@ public class Histograms extends ConsoleProgram {
 
 	}
 	
+
+
+
+
+
 }
--- week4/NameCounts.java
+++ week4/NameCounts.java
@@ -1,11 +1,55 @@
 package week4;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 import acm.program.ConsoleProgram;
 
 public class NameCounts extends ConsoleProgram {
 	
 	public void run() {
+		 Scanner input = new Scanner(System.in);
+         ArrayList <String> list = new ArrayList<String>();
+	            
+         		
+         	System.out.println("Enter name:");
+	            list.add(input.nextLine());
+	            System.out.println("Enter name:");
+	            list.add(input.nextLine());
 
+	            System.out.println("Enter name:");
+	            list.add(input.nextLine());
+
+	            System.out.println("Enter name:");
+	            list.add(input.nextLine());
+	            System.out.println("Enter name:");
+	            list.add(input.nextLine());
+	            System.out.println("Enter name:");
+	            list.add(input.nextLine());
+	            System.out.println("Enter name:");
+	            list.add(input.nextLine());
+	            System.out.println("Enter name:");
+	            list.add(input.nextLine());
+
+	          boolean isThere = true;
+	            
+	          
+	          for (String g : list) {
+
+	                if( input == null ) {
+	                isThere = false;
+	                return;
+	                }
+
+	               System.out.println("-------------------------------");
+	                System.out.println(g);
+
+
+	            }
+		
+		
+		
+		
 	}
 	
 }
